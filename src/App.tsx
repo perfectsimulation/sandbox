dab5fdee88824471dedc122819a8d424378d5e95
Sally Blair <sally@sallyblair.io>
2021-08-29 17:51:21 -0700

    Add CSS reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,94 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend, hr,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  input,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video
+  button {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    box-sizing: border-box;
+  }
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1.5;
+    font-size: 16px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+  h1 {
+    font-size: 4.5rem;
+    font-weight: 800;
+  }
+  a {
+    font-family: 'Libre Franklin';
+  }
+  button {
+    display: inline-block;
+    border: none;
+    outline: none;
+    text-decoration: none;
+    cursor: pointer;
+    appearance: none;
+    box-sizing: border-box;
+  }
+  input, select, textarea {
+    display: inline-block;
+    border: none;
+    text-decoration: none;
+    outline: none;
+    cursor: pointer;
+    appearance: none;
+    box-sizing: border-box;
+  }
+`;
 
 function App() {
   return (
-    <main
-      style={{
-        height: '100vh',
-        width: '100vw',
-        backgroundColor: '#000000',
-      }}
-    />
+    <>
+      <GlobalStyle />
+      <main
+        style={{
+          height: '100vh',
+          width: '100vw',
+          backgroundColor: '#000000',
+        }}
+      />
+    </>
   );
 }
 
